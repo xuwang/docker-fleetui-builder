15c4a53258ee246871ebfe9c932acce49213dfda
CoreOS Admin <core@ip-10-42-2-36.us-west-2.compute.internal>
2015-03-31 03:22:25 +0000

    fleetctl download path changed.

--- compile-build.sh
+++ compile-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #FLEETUIREPO=github.com/purpleworks/fleet-ui
-FLEETUIREPO=github.com/xuwang/fleet-ui
-FLEETCTL_VERSION=v0.9.1
+FLEETUIREPO=${FLEETUIREPO:-github.com/purpleworks/fleet-ui}
+FLEETCTL_VERSION=${FLEETCTL_VERSION:-v0.9.1}
 
 go get ${FLEETUIREPO} \
     && cp $GOPATH/bin/fleet-ui $GOPATH/src/${FLEETUIREPO}/tmp \
@@ -19,10 +19,12 @@ cd $GOPATH/src/${FLEETUIREPO}/angular \
 
 # Install fleetctl, and change the fleetctl version in fleet-ui's Dockefile to match
 cd $GOPATH/src/${FLEETUIREPO}; \
-    curl -s -L https://github.com/coreos/fleet/releases/download/${FLEETCTL_VERSION}/fleet-${FLEETCTL_VERSION}-linux-amd64.tar.gz | tar xz -C tmp/
+    curl -s -L https://github.com/coreos/fleet/releases/download/${FLEETCTL_VERSION}/fleet-${FLEETCTL_VERSION}-linux-amd64.tar.gz | tar xz -C tmp/ \
+    && cp tmp/fleet-${FLEETCTL_VERSION}-linux-amd64/fleetctl tmp 
+    
 
 tag=$(cd ${GOPATH}/src/${FLEETUIREPO}; git rev-parse --short=12 HEAD) 
 sed -i "s/fleet.*-linux-amd64/fleet-${FLEETCTL_VERSION}-linux-amd64/" ${GOPATH}/src/${FLEETUIREPO}/Dockerfile 
 
 docker build -t fleet-ui:$tag ${GOPATH}/src/${FLEETUIREPO} \
-    && docker tag fleet-ui:$tag fleet-ui:latest 
+    && docker tag -f fleet-ui:$tag fleet-ui:latest 
