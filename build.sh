e6fc677ce878d308292a253a639edf68f8ab8ac3
Xu Wang <xuwang@gmail.com>
2015-04-06 19:46:02 -0700

    remove old images before build the new ones

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 # First build the fleet-ui-builder image
 echo building fleet-ui-builder ...
-#docker rmi fleet-ui-builder:latest > /dev/null 2>&1
+docker rmi fleet-ui-builder:latest > /dev/null 2>&1
 docker build -t fleet-ui-builder .
 
 # Run the builder image to build fleet-ui docker image
-#docker rmi fleet-ui:latest > /dev/null 2>&1
+docker rmi fleet-ui:latest > /dev/null 2>&1
 echo building fleet-ui ...
-docker run -t --env FLEET_VERSION=v0.9.1 --env FLEETUIREPO=github.com/purpleworks/fleet-ui \
+docker run -t --env FLEET_VERSION=v0.9.1 --env FLEETUIREPO=github.com/xuwang/fleet-ui \
 	-v /var/run/docker.sock:/var/run/docker.sock fleet-ui-builder:latest
 
 # Show the build result
